6f05b42d742d12208100deabb28145ceef888159
thapakazi <milans.thapa78@gmail.com>
2019-04-06 15:21:17 +0545

    log - put logger, test tags

--- api/main.go
+++ api/main.go
@@ -11,8 +11,9 @@ func main() {
 
 	http.HandleFunc("/hostname", func(w http.ResponseWriter, r *http.Request) {
 		hostname, _ := os.Hostname()
-		log.Println("hello from " + hostname)
+		log.Println("serving from: " + hostname)
 		fmt.Fprint(w, "hello from "+hostname)
 	})
+
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
